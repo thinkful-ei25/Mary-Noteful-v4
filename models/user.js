03faee5a40fe3ad3a5fa75726166255aef28434c
Mary Conley <Mary@Marys-MacBook-Pro.local>
2018-10-29 14:24:09 -0700

    Finished Monday October 29th

---
+++ models/user.js
@@ -0,0 +1,35 @@
+'use strict'
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+//fullName string
+//username string required unique
+//password string required
+
+const userSchema = new mongoose.Schema({
+    fullName: String,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+    transform: (doc, result) => {
+      delete result._id;
+      delete result.__v;
+      delete result.password;
+    }
+  });
+
+  userSchema.methods.validatePassword = function (userSubmittedPassword) {
+    const user = this; 
+    return userSubmittedPassword === user.password;
+  };
+
+  userSchema.statics.hashPassword = function (incomingPassword) {
+    const digest = bcrypt.hash(incomingPassword, 10);
+    return digest;
+  };
+  
+  const userModel = mongoose.model('User', userSchema)
+  module.exports = userModel;
